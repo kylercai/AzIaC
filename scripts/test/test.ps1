f56e23179e26af234ff301eaa0f1d6252f4c7c5f
Kai Cai <kacai@microsoft.com>
2022-04-11 00:04:53 +0800

    4/11-01

---
+++ scripts/test/test.ps1
@@ -0,0 +1,16 @@
+param(
+     [Parameter()]
+     [string]$SubscriptionID,
+     [Parameter()]
+     [string]$Location
+ )
+
+#$Environment = "AzureCloud"
+$Environment = "AzureChinaCloud"
+
+$K2Group = "K2GRP"
+
+Connect-AzAccount -Environment  $Environment
+#az cloud set -n $Environment
+Set-AzContext -SubscriptionId $SubscriptionID 
+az group create -n $K2Group -l $Location
