b3e47ce11213b18f32b1b3ef11898e61555a3cea
Kai Cai <kacai@microsoft.com>
2022-04-10 16:41:11 +0800

    4/10-07

--- scripts/buildk2.ps1
+++ scripts/buildk2.ps1
@@ -7,7 +7,7 @@ param(
 
 $Environment = "AzureChinaCloud" #"AzureCloud"
 
-$K2Group = "K2RG"
+$K2Group = "K2IaC"
 $K2BackupGroup = "K2Backup"
 $K2BackupStorAcct = "k2sqlserverbackupstorage"
 
@@ -16,12 +16,12 @@ Connect-AzAccount -Environment  $Environment
 Set-AzContext -SubscriptionId $SubscriptionID 
 
 #创建网络基础环境，创建AKS环境
-./deploy-aks.ps1 -Location $Location -K2Group $K2Group
+#./deploy-aks.ps1 -Location $Location -K2Group $K2Group
 
 #恢复SQL Server数据库
-./deploy-sqlserver.ps1 -SubscriptionID $SubscriptionID -Location $Location -K2Group $K2Group -K2BackupGroup $K2BackupGroup -K2BackupStorAcct $K2BackupStorAcct
+#./deploy-sqlserver.ps1 -SubscriptionID $SubscriptionID -Location $Location -K2Group $K2Group -K2BackupGroup $K2BackupGroup -K2BackupStorAcct $K2BackupStorAcct
 
 #恢复MySQL数据库
 
 #恢复K2服务器
-#./deploy-k2server.ps1 -Location $Location -K2Group $K2Group
+./deploy-k2server.ps1 -Location $Location -K2Group $K2Group -K2BackupGroup $K2BackupGroup
--- scripts/deploy-k2server.ps1
+++ scripts/deploy-k2server.ps1
@@ -1,21 +1,26 @@
 param(
       [Parameter()]
+      [string]$Location,
+      [Parameter()]
       [string]$K2Group,
       [Parameter()]
-      [string]$Location
+      [string]$K2BackupGroup
  )
  
 $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 
 #镜像：k2-eng-image-20220329192619
-#/subscriptions/a15354eb-7827-4177-9745-7abe704ab8e9/resourceGroups/k2-aks/providers/Microsoft.Compute/images/k2-eng-image-20220329192619
+#/subscriptions/a15354eb-7827-4177-9745-7abe704ab8e9/resourceGroups/K2Backup/providers/Microsoft.Compute/images/k2-eng-image-20220329192619
+
+$ImageId = Get-AzResource -ResourceGroup $K2BackupGroup -Name k2-eng-image-20220329192619 -ResourceType Microsoft.Compute/images | Select-Object -expand ResourceId
+
 az vm create  `
   -n 'k2-en-1'  `
   -g $K2Group  `
-  --image 'k2-eng-image-20220329192619'  `
+  --image $ImageId  `
   --admin-username 'azureuser'  `
   --admin-password 'vanke@202203'  `
-  --size Standard D8s_v4  `
+  --size Standard_D8s_v4  `
   --vnet-name 'k2Vnet'  `
   --subnet 'k2WorkflowVMSubnet'  `
   -l $Location
