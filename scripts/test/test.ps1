a654775f14bb2538ec0699e5527523605f2a6c82
Kai Cai <kacai@microsoft.com>
2022-04-11 09:29:33 +0800

    4/11-02

--- scripts/test/test.ps1
+++ scripts/test/test.ps1
@@ -10,7 +10,8 @@ $Environment = "AzureChinaCloud"
 
 $K2Group = "K2GRP"
 
-Connect-AzAccount -Environment  $Environment
-#az cloud set -n $Environment
-Set-AzContext -SubscriptionId $SubscriptionID 
+#Connect-AzAccount -Environment  $Environment
+az cloud set -n $Environment
+#Set-AzContext -SubscriptionId $SubscriptionID 
+az account set -s $SubscriptionID
 az group create -g $K2Group -l $Location
