3a1cea66505f4c98b23ff9aa626cccf1d5db98be
任澍强 任 <h-rensq@vanke.com>
2022-04-13 07:44:03 +0000

    Updated buildk2.ps1

--- scripts/buildk2.ps1
+++ scripts/buildk2.ps1
@@ -12,6 +12,7 @@ $K2Group = "K2IaC2"
 $K2BackupGroup = "K2Backup"
 $K2BackupStorAcct = "k2sqlserverbackupstorage"
 $K2MySQLServer = "k2mysql01"
+$MYSQLLocation = "chinaeast2"
 
 $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 
@@ -30,7 +31,7 @@ az group create -l $Location -g $K2Group
 ./deploy-sqlserver.ps1 -SubscriptionID $SubscriptionID -Location $Location -K2Group $K2Group -K2BackupGroup $K2BackupGroup -K2BackupStorAcct $K2BackupStorAcct
 
 #恢复MySQL数据库
-./deploy-mysql.ps1 -SubscriptionID $SubscriptionID -Location chinaeast2 -K2MySQLServer $K2MySQLServer -K2Group $K2Group
+./deploy-mysql.ps1 -SubscriptionID $SubscriptionID -Location $MYSQLLocatio -K2MySQLServer $K2MySQLServer -K2Group $K2Group
 
 #恢复K2服务器
 ./deploy-k2engine.ps1 -SubscriptionID $SubscriptionID -Location $Location -K2Group $K2Group -K2BackupGroup $K2BackupGroup
